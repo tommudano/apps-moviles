15b900bc80a09b4b008939f2e63a473c645fbd25
tommudano <38632202+tommudano@users.noreply.github.com>
2022-09-28 15:04:01 -0300

    Code Formatting

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,15 +2,21 @@ import React from "react";
 import { View, Image, TouchableWithoutFeedback } from "react-native";
 import styles from "../styles/MenuStyles";
 
-const Menu = ({setModalVisible}) => {
+const Menu = ({ setModalVisible }) => {
     return (
         <View style={styles.navBar}>
-            <Image style={styles.apiLogo} source={require('../../assets/APILogo.png')}/>
+            <Image
+                style={styles.apiLogo}
+                source={require("../../assets/APILogo.png")}
+            />
             <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
-                <Image style={styles.filterLogo} source={require('../../assets/filter-solid.png')}/>
+                <Image
+                    style={styles.filterLogo}
+                    source={require("../../assets/filter-solid.png")}
+                />
             </TouchableWithoutFeedback>
         </View>
-    )
-}
+    );
+};
 
-export default Menu
+export default Menu;
--- src/styles/FilterDropDownRadioButtonStyles.js
+++ src/styles/FilterDropDownRadioButtonStyles.js
@@ -1,34 +1,34 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  dropDownContainer: {
-    paddingHorizontal: 20,
-    paddingTop: 30,
-  },
-  dropDownBody: {
-    backgroundColor: "#FFFFFF",
-    borderColor: "#ACACAC",
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-  },
-  dropDownInformation: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  dropDownDirectionIndicator: {
-    height: 25,
-    width: 25,
-  },
-  dropDownTitle: {
-    fontSize: 18,
-    fontFamily: "Roboto",
-    letterSpacing: -0.32,
-    fontWeight: "600",
-    color: "#8F8F8F",
-  },
+    dropDownContainer: {
+        paddingHorizontal: 20,
+        paddingTop: 30,
+    },
+    dropDownBody: {
+        backgroundColor: "#FFFFFF",
+        borderColor: "#ACACAC",
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+    },
+    dropDownInformation: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    dropDownDirectionIndicator: {
+        height: 25,
+        width: 25,
+    },
+    dropDownTitle: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+        letterSpacing: -0.32,
+        fontWeight: "600",
+        color: "#8F8F8F",
+    },
 });
 
 export default styles;
